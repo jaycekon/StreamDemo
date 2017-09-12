785402469fa9511b69feb10c6223cf02b5b299b2
jaycekon@163.com <jaycekong824>
2017-09-12 21:23:27 +0800

    整合代码

---
+++ src/main/java/com/Jaycekon/zookeeper/CreateGroup.java
@@ -0,0 +1,50 @@
+package com.Jaycekon.zookeeper;
+
+import org.apache.zookeeper.*;
+
+import java.io.IOException;
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * Created by Jaycekon on 2017/9/6.
+ */
+public class CreateGroup implements Watcher {
+
+    private ZooKeeper zk;
+    //会话延时
+    private static final int SESSION_TIMEOUT = 1000;
+
+    private CountDownLatch countDownLatch = new CountDownLatch(1);
+    public void process(WatchedEvent event) {
+        if(event.getState() == Event.KeeperState.SyncConnected){
+            countDownLatch.countDown();//计数器减一
+        }
+    }
+
+
+    public void connect(String hosts) throws IOException, InterruptedException {
+        zk = new ZooKeeper(hosts, SESSION_TIMEOUT, this);
+        countDownLatch.await();//阻塞程序继续执行
+    }
+
+    public void create(String groupName) throws KeeperException, InterruptedException {
+        String path = "/" + groupName;
+        String createPath = zk.create(path, null, ZooDefs.Ids.OPEN_ACL_UNSAFE/*允许任何客户端对该znode进行读写*/, CreateMode.PERSISTENT/*持久化的znode*/);
+        System.out.println("Created " + createPath);
+    }
+
+
+
+    public void close() throws InterruptedException {
+        if(zk != null){
+            try {
+                zk.close();
+            } catch (InterruptedException e) {
+                throw e;
+            }finally{
+                zk = null;
+                System.gc();
+            }
+        }
+    }
+}
---
+++ src/main/java/com/Jaycekon/zookeeper/CreateGroupTest.java
@@ -0,0 +1,50 @@
+package com.Jaycekon.zookeeper;
+
+import org.apache.zookeeper.KeeperException;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+
+/**
+ * Created by Jaycekon on 2017/9/6.
+ */
+public class CreateGroupTest {
+    private static String hosts = "192.168.2.105";
+    private static String groupName = "zoo";
+
+    private CreateGroup createGroup = null;
+
+    /**
+     * init
+     * @throws InterruptedException
+     * @throws KeeperException
+     * @throws IOException
+     */
+    @Before
+    public void init() throws KeeperException, InterruptedException, IOException {
+        createGroup = new CreateGroup();
+        createGroup.connect(hosts);
+    }
+
+    @Test
+    public void testCreateGroup() throws KeeperException, InterruptedException {
+        createGroup.create(groupName);
+    }
+
+    /**
+     * 销毁资源
+     */
+    @After
+    public void destroy() {
+        try {
+            createGroup.close();
+            createGroup = null;
+            System.gc();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
